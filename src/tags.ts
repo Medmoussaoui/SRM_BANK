c6af2d9634cf6d3602831bac0aefda0c0d926786
mohamed moussaoui <moussaouifilm16@gmail.com>
2022-09-01 09:36:48 +0100

    learn add second tag

--- src/tags.ts
+++ src/tags.ts
@@ -2,3 +2,6 @@ function myNewTag(version : number){
     console.log('Tag Version ' + version + ' v');
 }
 
+function mySecondTagVersion(version: number){
+    console.log('Test Add Second Tag Version');
+}
